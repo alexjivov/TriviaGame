fbd025a8e1927419b8101dc851b4efe554be3a4f
Alex Jivov <alexjivov@gwu.edu>
2018-08-03 22:20:12 -0400

    question array added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,10 +24,96 @@ const my Questions = [
 },
 
 {
-    question: ""
-}
-    
-    
+    question: "Who is the criminal mastermind main character of Blacklist?",
+    answers: {
+        a: "Raymond Reddington",
+        b: "Steven Seagal",
+        c: "General Kilgore",
+        d: "John Diefenbaker",
+    },
+    correctAnswer: "a"
+},
+{
+    question: "The famous speech from Blade Runner is called Tears in the _____",
+    answers: {
+        a: "Snow",
+        b: "Sun",
+        c: "Rain",
+        d: "Tornado",
+    },
+    correctAnswer: "c"
+},
+{
+    question: "What drug is Bryan Cranston's character in Breaking Bad very good at making?",
+    answers: {
+        a: "Cocaine",
+        b: "Meth",
+        c: "Heroin",
+        d: "MDMA",
+    },
+    correctAnswer: "b"
+},
+{
+    question: "Frank Underwood begins in what government position in the tv show House of Cards?",
+    answers: {
+        a: "Majority Whip",
+        b: "Senate Aide",
+        c: "Vice President",
+        d: "President",
+    },
+    correctAnswer: "a"
+},
+{
+    question: "What is the name of Anthony Bourdain's most recent food and travel show?",
+    answers: {
+        a: "Parts Unknown",
+        b: "Kitchen Confidential",
+        c: "Diners, Drive-Ins and Dives",
+        d: "Mind of a Chef",
+    },
+    correctAnswer: "a"
+},
+{
+    question: "Who is Tony Soprano's wife in The Sopranos?",
+    answers: {
+        a: "Meadow",
+        b: "Christina Melfi",
+        c: "Gloria",
+        d: "Carmella",
+    },
+    correctAnswer: "d"
+},
+{
+    question: "What Disney movie has the Dwayne the Rock Johnson playing a Hawaiian demi-god?",
+    answers: {
+        a: "Hercules",
+        b: "Dumbo",
+        c: "Moana",
+        d: "Lilo and Stitch",
+    },
+    correctAnswer: "c"
+},
+{
+    question: "Who is the most famous nemesis of Dexter Morgan's in the tv show Dexter?",
+    answers: {
+        a: "The Doomsday Killer",
+        b: "The Trinity Killer",
+        c: "Sergeant Doakes",
+        d: "Detective Bautista",
+    },
+    correctAnswer: "b"
+},
+{
+    question: "Who marries Monica in the tv show friends?",
+    answers: {
+        a: "Ross",
+        b: "Joey",
+        c: "Chandler",
+        d: "Richard",
+    },
+    correctAnswer: "c"
+},
     
     }
-]
+];
+
